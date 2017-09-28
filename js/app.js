fa91d58c6eb4c408311b84e244e8c89d09e4df35
Heming Yuan <yhmbkycanu@gmail.com>
2017-09-29 09:47:46 +1000

    feat: add q1-q4 in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("Hello, World")
+//q1
+console.log("Hello, World");
+//q2
+console.log("The answer for q2 is a");
+//q3
+var myName = "袁鹤鸣";
+console.log(myName);
+//q4
+
+window.onload = function(){
+    alert( myName );
+    var name = document.querySelector('h1');
+    name.onclick(){
+        var content = "<span>I\'m</span>" + myName;
+        name.setAttribute("content", content);
+    }
+
+};
+
+
+
